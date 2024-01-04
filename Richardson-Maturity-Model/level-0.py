43b53862eb380eed453aed1617254df9ecb4ebf8
Baiquan Fan <baiquan.fan@outlook.com>
2024-01-04 19:21:51 +0200

    add example for Rechardson Maturity Model level 0.

---
+++ Richardson-Maturity-Model/level-0.py
@@ -0,0 +1,54 @@
+from ctypes import Union
+from typing import Annotated, Optional
+from fastapi import Body, FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Payload(BaseModel):
+    items: str | None = None
+    users: str | None = None
+
+# Example in-memory data (pretending to be a database)
+fake_db = {
+    "items": [],
+    "users": []
+}
+
+@app.post("/data")
+async def create_data(payload: Annotated[
+        Payload, Body(openapi_examples={
+                "items": {
+                    "summary": "payload to create an item",
+                    "description": "Create an item.",
+                    "value": {
+                        "items": "item1",
+                    },
+                },
+                "users": {
+                    "summary": "payload to create an user",
+                    "description": "Create an user",
+                    "value": {
+                        "users": "user1",
+                    },
+                },
+                "invalid": {
+                    "summary": "Invalid data is rejected with an error",
+                    "description": "Invalid payload",
+                    "value": {
+                        "item": 123
+                    },
+                },
+            },
+        ),
+    ],
+):
+    """Create data for a specified resource"""
+    if payload.items:
+        fake_db["items"].append(payload.items)
+    elif payload.users:
+        fake_db["users"].append(payload.users) 
+    else:
+        raise HTTPException(status_code=404, detail="Resource not found")   
+
+    return fake_db
